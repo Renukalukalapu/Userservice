bfbef7726c26dcf44178129aec09b66455d52aa6
lukalapurenuka978 <lukalapurenuka978@gmail.com>
2025-02-12 14:12:35 +0530

    completed manual authentication

---
+++ src/main/java/com/userservice/user/exceptions/Usernotfoundexception.java
@@ -0,0 +1,4 @@
+package com.userservice.user.exceptions;
+
+public class Usernotfoundexception {
+}
---
+++ src/main/java/com/userservice/user/repositories/TokenRepository.java
@@ -0,0 +1,4 @@
+package com.userservice.user.repositories;
+
+public interface TokenRepository {
+}
